2dce246de4fd8ccbc07e14ab39c2a0ddb0001d99
Thales <thaleesresende@gmail.com>
2025-11-25 23:04:50 -0300

    adiciona logs de debug

--- src/cpu.py
+++ src/cpu.py
@@ -7,8 +7,10 @@ class CPU(MemoryLoader):
     """
 
     def __init__(self):
+        # Inicializa a memória e estados (feito pelo MemoryLoader)
         super().__init__()
-        # Mapeamento para debug 
+        
+        # Mapeamento pra debug
         self.OPCODE_NAMES = {
             1: "ADD", 2: "SUB", 3: "ZEROS", 4: "XOR", 5: "OR", 6: "NOT",
             7: "AND", 8: "ASL", 9: "ASR", 10: "LSL", 11: "LSR", 12: "COPY",
@@ -17,30 +19,34 @@ class CPU(MemoryLoader):
             255: "HALT" 
         }
 
-    def run(self): 
+    def run(self): # Loop principal do processador. Executa instruções até encontrar a parada (HALT)
+    
         print(f"--- Iniciando Execução (PC Inicial: {self.state.pc}) ---")
         
         cycle_count = 0
-        # Loop principal
+       
+        # O loop roda enquanto halted for FALSE
         while not self.state.halted:
             self.step()
             cycle_count += 1
             
-            
+            # Limite para impedir loop infinito
             if cycle_count > 1000:
-                print("AVISO: Limite de ciclos atingido (Loop infinito?)")
+                print("AVISO: Limite de ciclos de segurança atingido (Loop infinito?)")
                 break
         
         print(f"--- Execução Finalizada em {cycle_count} ciclos ---")
 
-   def step(self):
-        # 1. ESTÁGIO IF (Instruction Fetch)
+    
+    def step(self): #Ciclo completo de instrução ( IF,ID,EX,WB)
+        
+        # 1. ESTÁGIO IF (Instruction Fetch) - Busca
         current_pc = self.state.pc
         instrucao = self.fetch_instruction() 
         self.incr_pc() 
 
-        # 2. ESTÁGIO ID (Instruction Decode)
-        
+        # 2. ESTÁGIO ID (Instruction Decode) - Decodificação
+
         # Tratamento antecipado do HALT (evita ler reg 255 inválido)
         if instrucao == 0xFFFFFFFF:
             self.state.halted = True
@@ -52,48 +58,79 @@ class CPU(MemoryLoader):
         ra_idx = self.extract_field(instrucao, 16, 23)
         rb_idx = self.extract_field(instrucao, 8, 15)
         rc_idx = self.extract_field(instrucao, 0, 7)
+        
         const16 = self.extract_field(instrucao, 8, 23)
         addr24  = self.extract_field(instrucao, 0, 23)
 
         # Leitura dos registradores (Busca de operandos)
         val_ra = self.read_reg(ra_idx)
         val_rb = self.read_reg(rb_idx)
+        
 
-        # 3. ESTÁGIO EX/MEM e WB
+
+        # 3. ESTÁGIO EX/MEM (Execução) e 4. WB (Write Back)
+        
+        # Debug 
+        #nome_instrucao = self.OPCODE_NAMES.get(opcode, "DESCONHECIDO")
+        #print(f"[DEBUG P3] Ciclo em PC={current_pc}: Inst={nome_instrucao}")
+
         self.execute_instruction(opcode, ra_idx, rb_idx, rc_idx, 
                                  val_ra, val_rb, const16, addr24, current_pc)
 
     def execute_instruction(self, opcode, ra, rb, rc, val_ra, val_rb, const16, addr24, current_pc):
-        # --- Instruções ALU (Esqueleto para a Pessoa 4) ---
+
+        # debug pra if e id
+        #nome_instrucao = self.OPCODE_NAMES.get(opcode, "DESCONHECIDO")
+        #print(f"[DEBUG P3] Ciclo em PC={current_pc}:")
+       #print(f"   -> Instrução: {nome_instrucao} (Opcode {opcode})")
+        #print(f"   -> Registradores decodificados: Ra=R{ra}, Rb=R{rb}, Rc=R{rc}")
+        #print(f"   -> Valores lidos: Val_Ra={val_ra}, Val_Rb={val_rb}")
+        #print("-" * 40)
+        # ------------------------------------
+        
+        # --- Instruções ALU
         if opcode == 1: # ADD
             pass
+            
         elif opcode == 2: # SUB
             pass
+            
         elif opcode == 16: # LOAD
             pass
+            
         elif opcode == 17: # STORE
             pass
+            
         elif opcode == 22: # JUMP
             pass
+
         else:
+            # Se não é HALT e não conhecemos o opcode, é uma instrução desconhecida (ou NOP)
             pass
 
 # -----------------------------------------------------------------------------
-# Bloco de Teste Rápido
+# Bloco de Teste Rápido (Só roda se executar este arquivo diretamente)
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
     import os
+    
+    # Arquivo binário de entrada ( instruções)
     bin_file = "programa.bin"
     
     if os.path.exists(bin_file):
         print("Arquivo programa.bin encontrado. Iniciando teste da CPU...")
+        
         cpu = CPU()
-        cpu.load_from_file(bin_file, verbose=True)
+        cpu.load_from_file(bin_file, verbose=True) # carrega a memória
+        
+        # Executa a simulação
         cpu.run()
         
+        # Mostra estado final
         print("\nEstado Final dos Registradores:")
         for r in cpu.dump_registers():
-            if r[1] != 0:
+            if r[1] != 0: # Só mostra quem não é zero
                 print(f"R{r[0]}: {r[1]} (Int: {r[2]})")
+                
     else:
         print(f"ERRO: {bin_file} não encontrado")
